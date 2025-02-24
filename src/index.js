821d030cef5aa919ff071b0f5aaaabdcd4807ba3
emilyrh <114513651+emilyrh@users.noreply.github.com>
2025-02-24 20:07:30 +0000

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayAffirmation(response) {
-  console.log("Poem generated");
   new Typewriter("#affirmation", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,14 +12,14 @@ function generateAffirmation(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "a12670296b5b7ac064111tf40b315o03";
-  let prompt = `User instructions: Generate a positive affirmation about ${instructionsInput.value}. Just the affirmation please.`;
+  let prompt = `User instructions: Generate three different positive affirmations about ${instructionsInput.value}. Only include the affirmations as sentences, no numbers or bullet points please.`;
   let context =
     "You are a positivity life coach and love to write positive affirmations. Your mission is to generate positive affirmations. Make sure to follow the user instructions.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let affirmationElement = document.querySelector("#affirmation");
+  affirmationElement.classList.remove("hidden");
+  affirmationElement.innerHTML = `<div class ="generating">Generating positive affirmations for ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayAffirmation);
 }
