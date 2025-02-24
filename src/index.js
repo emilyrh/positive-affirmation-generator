07350d36416187206c893bd44fc2696ad001176a
emilyrh <114513651+emilyrh@users.noreply.github.com>
2025-02-24 18:41:20 +0000

    Added basic js form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateAffirmation(event) {
+  event.preventDefault();
 
+  new Typewriter("#affirmation", {
+    strings: ["Affirmation will go here"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let affirmationFormElement = document.querySelector(
+  "#affirmation-generator-form"
+);
+affirmationFormElement.addEventListener("submit", generateAffirmation);
