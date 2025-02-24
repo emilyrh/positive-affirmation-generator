c07a08266b6515fb0f4a46fe62ceac3d737de022
emilyrh <114513651+emilyrh@users.noreply.github.com>
2025-02-24 19:11:03 +0000

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateAffirmation(event) {
-  event.preventDefault();
-
+function displayAffirmation(response) {
+  console.log("Poem generated");
   new Typewriter("#affirmation", {
-    strings: ["Affirmation will go here"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateAffirmation(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a12670296b5b7ac064111tf40b315o03";
+  let prompt = `User instructions: Generate a positive affirmation about ${instructionsInput.value}. Just the affirmation please.`;
+  let context =
+    "You are a positivity life coach and love to write positive affirmations. Your mission is to generate positive affirmations. Make sure to follow the user instructions.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayAffirmation);
 }
 
 let affirmationFormElement = document.querySelector(
